0893bba657dd8af1b9704cbd613bd578022a65da
seongkyun <gyun59@gmail.com>
2022-05-23 19:09:16 +0900

    [update .gitignore]

--- src/test/java/com/devlop/skfirst/web/HelloControllerTest.java
+++ src/test/java/com/devlop/skfirst/web/HelloControllerTest.java
@@ -2,14 +2,16 @@ package com.devlop.skfirst.web;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,8 +25,21 @@ public class HelloControllerTest {
         String hello ="hello";
 
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(hello));
 
+    }
+
+    @Test
+    public void helloDtoTest() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name",name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
