1582055ee3b36581d7d264aaa21af769e6f4a9c6
seongkyun <gyun59@gmail.com>
2022-05-25 20:08:44 +0900

    커밋 moorim mustache

---
+++ src/test/java/com/devlop/skfirst/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.devlop.skfirst.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainpage_loading(){
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        assertThat(body).contains("하윤 웹서비스");
+    }
+}
