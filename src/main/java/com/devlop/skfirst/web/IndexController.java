37290450cf4f8146121861b78c695fb9ff4b6821
seongkyun <gyun59@gmail.com>
2022-05-26 20:41:23 +0900

    커밋 moorim mustache

--- src/main/java/com/devlop/skfirst/web/IndexController.java
+++ src/main/java/com/devlop/skfirst/web/IndexController.java
@@ -1,12 +1,19 @@
 package com.devlop.skfirst.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
